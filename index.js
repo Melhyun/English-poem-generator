5a0536abc74bb805c7e301985aef596ea6a9e5ac
Melhyun <melika.bastani@gmail.com>
2024-10-29 00:30:15 +0330

    Integrated the API in js

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-function generate(event) {
-  event.preventDefault();
+function poem(response) {
   new Typewriter(".poem", {
-    strings: "Hold fast to dreams For if dreams die",
+    strings: response.data.answer,
+    cursor: null,
     delay: 1,
     autoStart: true,
   });
+}
+
+function generate(event) {
+  event.preventDefault();
+
+  let userInstruction = document.querySelector("#user-instruction");
+
+  apiKey = "59c94t1918f34a2b8290c703o6bece67";
+  let prompt = `generate a poem with the topic ${userInstruction.value}`;
+  let context = `You are a romantic poet, write a 4 line english poem  with ${userInstruction.value},seperate the lines between the poem with <br/> , add the Shecodes AI signature at the bottom of it on a seperate line and put it in <strong>, seperate the poem and the signature`;
+  apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(poem);
 }
 
 let poemForm = document.querySelector("#poem-form");
