6715b7b57a94a8a1cd2785d584213b8a9d7de455
Melhyun <melika.bastani@gmail.com>
2024-10-30 00:43:34 +0330

    improved the UX

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ function generate(event) {
 
   let userInstruction = document.querySelector("#user-instruction");
 
+  let poemElement = document.querySelector(".poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = ` Generating an English poem about ${userInstruction.value}`;
+
   apiKey = "59c94t1918f34a2b8290c703o6bece67";
   let prompt = `generate a poem with the topic ${userInstruction.value}`;
   let context = `You are a romantic poet, write a 4 line english poem  with ${userInstruction.value},seperate the lines between the poem with <br/> , add the Shecodes AI signature at the bottom of it on a seperate line and put it in <strong>, seperate the poem and the signature`;
